ce9f84d09b8957075af2002c036036b37cea9b83
novak <novak@netsearch.cz>
2026-02-05 09:42:59 +0100

    working as package

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nix build/dev environment for aff4-cpp-lite";
+  description = "AFF4 C++ Lite (proper Nix package)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
@@ -8,59 +8,54 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
+
       let
         pkgs = import nixpkgs { inherit system; };
 
-        raptor2Pkg = if pkgs ? raptor2 then pkgs.raptor2 else pkgs.librdf_raptor2;
+        raptor2Pkg =
+          if pkgs ? raptor2 then pkgs.raptor2 else pkgs.librdf_raptor2;
 
-        aff4 = pkgs.stdenv.mkDerivation {
-          pname = "aff4-cpp-lite";
-          version = "2.0.6";
-          src = self;
-
-          nativeBuildInputs = with pkgs; [
-            autoreconfHook
-            pkg-config
-            libtool
-            autoconf
-            automake
-            doxygen
-          ];
-
-          buildInputs = with pkgs; [
-            zlib
-            snappy
-            lz4
-            raptor2Pkg
-            cppunit
-            openssl
-          ];
-
-          configureFlags = [
-            "--enable-debug=no"
-          ];
-
-          doCheck = true;
-
-          meta = with pkgs.lib; {
-            description = "Lightweight C++11 implementation of AFF4";
-            homepage = "https://github.com/Velocidex/c-aff4";
-            license = licenses.lgpl3Plus;
-            platforms = platforms.unix;
-          };
-        };
       in {
-        packages = {
-          default = aff4;
-          aff4-cpp-lite = aff4;
-        };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ aff4 ];
-          packages = with pkgs; [
-            gdb
-            clang-tools
-          ];
-        };
+        packages.default =
+          pkgs.stdenv.mkDerivation {
+            pname = "aff4-cpp-lite";
+            version = "2.0.6";
+
+            src = self;
+
+            nativeBuildInputs = with pkgs; [
+              autoreconfHook
+              pkg-config
+              autoconf
+              automake
+              libtool
+            ];
+
+            buildInputs = with pkgs; [
+              zlib
+              snappy
+              lz4
+              raptor2Pkg
+              cppunit
+              openssl
+            ];
+
+            configureFlags = [
+              "--enable-static"
+              "--enable-shared"
+            ];
+
+            enableParallelBuilding = true;
+
+            doCheck = false;
+
+            meta = with pkgs.lib; {
+              description = "AFF4 C++ implementation";
+              license = licenses.lgpl3Plus;
+              platforms = platforms.unix;
+            };
+          };
+
       });
 }
