a3468400007fbf6abb8ff0a63d9c73397fd607d8
Jan Pluskal <jan.pluskal@gmail.com>
2026-02-05 15:15:12 +0100

    Pin flake to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "AFF4 C++ Light forensic container library";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
